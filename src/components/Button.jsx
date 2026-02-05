1fc2f18b99044c10e9994153acac5af9cbfae0c6
Bedant186 <bedantsinghania186@gmail.com>
2026-02-05 17:03:04 +0530

    Delete src directory

--- src/components/Button.jsx
+++
@@ -1,6 +0,0 @@
-const Button = ({ text, onClick }) => (
-  <button onClick={onClick} style={{ padding: "10px 20px", background: "#007bff", color: "#fff", border: "none", borderRadius: "5px", cursor: "pointer", marginTop: "10px" }}>
-    {text}
-  </button>
-);
-export default Button;
